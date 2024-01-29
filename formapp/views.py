7a3012a09da08adc4202a7a3ed18db6525b2241a
安齋宇宙 <anzaiuchu@ip-10-95-3-99.ap-northeast-1.compute.internal>
2024-01-29 10:33:08 +0900

    Post

--- formapp/views.py
+++ formapp/views.py
@@ -16,16 +16,3 @@ class FormClass(CreateView):
     model = PostModel
     success_url = reverse_lazy('formapp:list')
     form_class = PostForm
-
-    # def form_valid(self, form):
-    #     print(form.cleaned_data.get("image"))
-    #     postform = PostForm(image=form.cleaned_data.get("image"), title=form.cleaned_data.get("title"))
-    #     if postform.is_valid():
-    #         post = PostModel()
-    #         # post.image = form.FILES['image']
-    #         post.save()
-
-    #     pd = postform.save(commit=False)
-    #     pd.user = self.request.user
-    #     pd.save()
-    #     return super().form_valid(postform)
