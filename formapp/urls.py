a2f1d76daf36247bc2366ff56b0c9398d6e4d992
安齋宇宙 <anzaiuchu@ip-10-95-3-99.ap-northeast-1.compute.internal>
2024-01-29 14:34:41 +0900

    delete

--- formapp/urls.py
+++ formapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import FormClass, ListClass
+from .views import FormClass, ListClass, deletePost
 
 app_name = 'formapp'
 urlpatterns = [
     path('', FormClass.as_view(), name='form'),
     path('list/', ListClass.as_view(), name='list'),
+    path('<str:pk>/delete/', deletePost, name='deletePost'),
 ]
