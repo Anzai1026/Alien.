e39c43cb0b614169de92cbe8efe2b83e195d7cb9
安齋宇宙 <anzaiuchu@anzaisoranoMacBook-Pro.local>
2024-01-30 17:27:57 +0900

    createdelte

--- formapp/views.py
+++ formapp/views.py
@@ -2,6 +2,8 @@ from django.views.generic import ListView, CreateView
 from django.urls import reverse_lazy
 from .models import PostModel
 from .forms import PostForm
+from rest_framework.response import Response
+from django.shortcuts import redirect
 
 class ListClass(ListView):
     template_name = 'list.html'
@@ -16,3 +18,10 @@ class FormClass(CreateView):
     model = PostModel
     success_url = reverse_lazy('formapp:list')
     form_class = PostForm
+
+def deletePost(request, pk):
+    print(pk)
+    post = PostModel.objects.get(id=pk)
+    post.delete()
+
+    return redirect('formapp:list')
